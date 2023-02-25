277316acb7ef45d7c6723a76faeaff50d78ac3c4
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-02-25 10:49:36 +0900

    [update] 弾の微小な更新

--- Assets/Game/Bullets/Script/01Bullets/ReflectBullet.cs
+++ Assets/Game/Bullets/Script/01Bullets/ReflectBullet.cs
@@ -8,17 +8,7 @@ namespace Bullet
     [System.Serializable]
     public class ReflectBullet : BulletBase
     {
-        [TagName, SerializeField]
-        private string _wallTag = default;
-
         public override BulletType Type => BulletType.ReflectBullet;
-
-        private Vector2 _velocity = default;
-
-        public void Update()
-        {
-            _velocity = _rigidbody2D.velocity;
-        }
 
         protected override void OnHitCollision(Collision2D target)
         {
