021a4d17c3e397e1cf5abba4653f58dc35db9d63
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-13 15:45:56 +0900

    [update] 反射弾を更新。反射方法を変更

--- Assets/Game/Bullets/Script/01Bullets/ReflectBullet.cs
+++ Assets/Game/Bullets/Script/01Bullets/ReflectBullet.cs
@@ -8,22 +8,60 @@ namespace Bullet
     [System.Serializable]
     public class ReflectBullet : BulletBase
     {
+        [Tooltip("最大何回壁を反射するか"), SerializeField]
+        private int _maxWallCollisionCount = 0;
+        [TagName, SerializeField]
+        private string _wallTag = default;
+        [SerializeField]
+        private LayerMask _wallLayer = default;
+
+        private CircleCollider2D _circleCollider2D = null;
+        /// <summary> 現在の反射回数 </summary>
+        private int _reflexCount = 0;
+
         public override BulletType Type => BulletType.ReflectBullet;
+
+        protected override void Start()
+        {
+            base.Start();
+            _circleCollider2D = GetComponent<CircleCollider2D>();
+        }
 
         protected override void OnHitCollision(Collision2D target)
         {
-            // ダメージを加える
-            if (target.collider.TryGetComponent(out IDamageable hit))
-            {
-                hit.Damage(_attackPower);
-                // 自身を破棄する
-                Destroy(this.gameObject);
-            }
-        }
 
+        }
         protected override void OnHitTrigger(Collider2D target)
         {
+            // ダメージを加える
+            if (target.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.Damage(_attackPower);
+            }
+            if (target.tag == _wallTag)
+            {
+                _reflexCount++;
 
+                if (_reflexCount >= _maxWallCollisionCount)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    // 自分の位置に、自分より一回り大きいサークルキャストを発生させる。
+                    var hit = Physics2D.CircleCast(this.transform.position,
+                        _circleCollider2D.radius * transform.localScale.x * 1.1f,
+                        Vector2.zero, 0f, _wallLayer);
+                    if (hit.collider == null)
+                    {
+                        Debug.LogWarning("レイがコライダーにヒットしませんでした。");
+                        return;
+                    }
+                    // 移動方向ベクトルを反射する。
+                    _rigidbody2D.velocity = Vector2.Reflect(_rigidbody2D.velocity, hit.normal);
+
+                }
+            }
         }
     }
 }
