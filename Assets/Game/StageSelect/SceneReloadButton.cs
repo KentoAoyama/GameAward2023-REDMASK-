fd42ac3fa4104167260498a907a2c91c5c8afbaa
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-17 22:35:32 +0900

    [add] Scene再読み込みボタン用コンポーネントを追加

---
+++ Assets/Game/StageSelect/SceneReloadButton.cs
@@ -0,0 +1,18 @@
+// 日本語対応
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneReloadButton : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            DOTween.KillAll();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+
+    }
+}
