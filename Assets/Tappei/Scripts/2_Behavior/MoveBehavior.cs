798dfaf1e20c62aa86b9b20d76753f50308e6045
SatouTappei <stutpi.signup@gmail.com>
2023-03-20 22:26:28 +0900

    [fix] MoveBehaviorのエラー修正

--- Assets/Tappei/Scripts/2_Behavior/MoveBehavior.cs
+++ Assets/Tappei/Scripts/2_Behavior/MoveBehavior.cs
@@ -27,7 +27,7 @@ public class MoveBehavior : MonoBehaviour
     [SerializeField] private Transform _spriteTrans;
     [SerializeField] private Transform _eyeTrans;
     [Header("°‚ğŒŸ’m‚·‚é‚½‚ß‚ÌRay‚Ìİ’è")]
-    [SerializeField] private LayerMask _groundLayerMask = LayerMask.NameToLayer("Ground");
+    [SerializeField] private LayerMask _groundLayerMask;
     [SerializeField] private float _rayDistance = 1.0f;
 
     private CancellationTokenSource _cts;
