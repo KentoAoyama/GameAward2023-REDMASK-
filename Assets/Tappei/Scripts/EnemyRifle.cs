6fee47c1922bfc49f2f678db7fe111ad1d2c3e4c
SatouTappei <stutpi.signup@gmail.com>
2023-03-22 11:19:20 +0900

    [update] IDamageableインターフェース周りの処理の修正を優先するために途中で一旦保存

--- Assets/Tappei/Scripts/EnemyRifle.cs
+++ Assets/Tappei/Scripts/EnemyRifle.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -6,10 +7,25 @@ using UnityEngine;
 public class EnemyRifle : MonoBehaviour, IEnemyWeapon
 {
     [Header("“G’e")]
-    [SerializeField] EnemyBullet _enemyBullet;
+    [SerializeField] private EnemyBullet _enemyBullet;
+    [Header("ƒv[ƒ‹‚·‚é“G’e‚Ì”")]
+    [SerializeField] private int _poolQuantity;
+
+    private Stack<EnemyBullet> _pool = new Stack<EnemyBullet>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < _poolQuantity; i++)
+        {
+            EnemyBullet bullet = Instantiate(_enemyBullet, transform.position, Quaternion.identity);
+            bullet.transform.position = Vector3.one * 100;
+            _pool.Push(bullet);
+        }
+    }
 
     public void Attack()
     {
         // ƒIƒuƒv[‚©‚ç’e‚ğŒÄ‚Ño‚·
+
     }
 }
