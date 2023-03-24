74965064861439357ed5184297fde35b27ee2b0a
Aiba_0325 <bantan.ab325@gmail.com>
2023-03-24 15:36:18 +0900

    [Updata] カメラの振動の、一時停止/再開　処理の追加

--- Assets/Game/Player/Script/02Behavior/CameraShake.cs
+++ Assets/Game/Player/Script/02Behavior/CameraShake.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Cysharp.Threading.Tasks;
 
 /// <summary>カメラの制御</summary>
 namespace Player
@@ -13,18 +14,35 @@ namespace Player
         private PlayerController _playerController;
 
         private CinemachineImpulseSource _source;
+        CinemachineImpulseListener impulseListener;
+
+        Vector3 saveVelo;
 
         public void Init(PlayerController playerController)
         {
             _playerController = playerController;
             _source = _playerController.Camera.GetComponent<CinemachineImpulseSource>();
+            impulseListener = _playerController.Camera.GetComponent<CinemachineImpulseListener>();
         }
 
 
         public void RevolverShootShake()
         {
             _source.GenerateImpulse();
-            Debug.Log("振動");
+
         }
+
+        public async void Pause()
+        {
+            await UniTask.WaitUntil(() => _playerController != null);
+            Debug.Log("カメラの振動をストップ");
+            impulseListener.enabled = false;
+        }
+        public void Resume()
+        {
+            Debug.Log("カメラの振動を再生");
+            impulseListener.enabled = true;
+        }
+
     }
 }
