251b0af3cbfc6f2803436a7b0a50690461c0eed9
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-27 21:02:48 +0900

    [update] 続きからボタン用コンポーネント作成

---
+++ Assets/Game/Title/TitleContinueButton.cs
@@ -0,0 +1,35 @@
+// 日本語対応
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// タイトルの続きからボタン
+/// </summary>
+public class TitleContinueButton : MonoBehaviour
+{
+    [SceneName, SerializeField]
+    private string _stageSelectButton = default;
+
+    public void Awake()
+    {
+        if (GameManager.Instance.CompletedStageManager.
+            LoadStageCompleteNumber() < 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        GetComponent<Button>().onClick.AddListener(OnSceneChange);
+    }
+    /// <summary>
+    /// ボタンを押下したときに呼び出すことを想定して作成したメソッド。<br/>
+    /// シーンを変更する。
+    /// </summary>
+    public void OnSceneChange()
+    {
+        GameManager.Instance.PauseManager.ClearCount();
+        DOTween.KillAll();
+        SceneManager.LoadScene(_stageSelectButton);
+    }
+}
