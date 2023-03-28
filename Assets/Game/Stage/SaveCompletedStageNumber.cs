375be645487eb1ee1dff304703004dc00235e585
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-28 17:39:46 +0900

    [update] 完了済みステージ番号を保存する用コンポーネントを作成

---
+++ Assets/Game/Stage/SaveCompletedStageNumber.cs
@@ -0,0 +1,16 @@
+// 日本語対応
+using UnityEngine;
+
+public class SaveCompletedStageNumber : MonoBehaviour
+{
+    [SerializeField]
+    private int _stageNumber = 0;
+
+    public void OnSave()
+    {
+        // 完了済みステージ番号を更新する。
+        GameManager.Instance.CompletedStageManager.SetCompletedStage(_stageNumber);
+        // 状態を保存する。
+        GameManager.Instance.CompletedStageManager.SaveStageCompleteNumber();
+    }
+}
