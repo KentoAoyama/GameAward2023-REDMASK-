a43445c45870a2bf0c08b21c38c2bde74e7c3119
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-03-28 17:37:05 +0900

    [update] ステージ1演出用ファイルを作成

---
+++ Assets/Game/Stage/Stage 1/StageOnePlayerDeadPerformance.cs
@@ -0,0 +1,10 @@
+// 日本語対応
+using Cysharp.Threading.Tasks;
+
+public class StageOnePlayerDeadPerformance : PlayerDeadEventBase
+{
+    protected override UniTask PlayerDeadPerformance()
+    {
+        throw new System.NotImplementedException();
+    }
+}
