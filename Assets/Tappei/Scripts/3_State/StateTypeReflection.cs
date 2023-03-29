04a1d88892fa77ccaed038467a3c380ed1d27bd6
SatouTappei <stutpi.signup@gmail.com>
2023-03-29 15:39:30 +0900

    [add] 盾持ち用の敵のステートのスクリプトを追加

---
+++ Assets/Tappei/Scripts/3_State/StateTypeReflection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ‚‚¿—p
+/// ‚‚ÉUŒ‚‚ğó‚¯‚Ä‚µ‚Î‚ç‚­d’¼‚µ‚Ä‚¢‚éó‘Ô
+/// </summary>
+public class StateTypeReflection : StateTypeBase
+{
+    public StateTypeReflection(EnemyController controller, StateType stateType)
+    : base(controller, stateType) { }
+}
