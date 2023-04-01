0d11cbd2c0eae10cc6e48219d9c1e78238287971
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-01 15:41:45 +0900

    [update] シールドの貫通に関係する処理を各弾に追加

--- Assets/Game/Bullets/Script/01Bullets/PenetrateBullet2.cs
+++ Assets/Game/Bullets/Script/01Bullets/PenetrateBullet2.cs
@@ -9,9 +9,12 @@ namespace Bullet
     [Serializable]
     public class PenetrateBullet2 : Bullet2
     {
-        [SerializeField]
+        [Tooltip("壁を貫通する回数"), SerializeField]
         private int _maxWallHitNumber = 1;
 
         public int MaxWallHitNumber => _maxWallHitNumber;
+
+        /// <summary> 貫通弾は盾を貫通する </summary>
+        protected override bool IsPenetrateShield => true;
     }
 }
--- Assets/Game/Bullets/Script/01Bullets/StandardBullet2.cs
+++ Assets/Game/Bullets/Script/01Bullets/StandardBullet2.cs
@@ -1,11 +1,11 @@
-using UnityEngine;
-using System;
+﻿using System;
 
 namespace Bullet
 {
     [Serializable]
     public class StandardBullet2 : Bullet2
     {
-
+        /// <summary> 通常弾は盾を貫通しない </summary>
+        protected override bool IsPenetrateShield => false;
     }
 }
