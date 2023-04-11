20db574d8b007037aa5e269ab9b379b3d4b24d0a
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-11 17:24:32 +0900

    [update] ステージコントローラー 直前からやり直す場合用に処理を追加

--- Assets/Game/Stage/StageController.cs
+++ Assets/Game/Stage/StageController.cs
@@ -1,8 +1,12 @@
 // 日本語対応
+using Player;
 using UnityEngine;
 
 public class StageController : MonoBehaviour
 {
+    [Header("プレイヤーのタグ")]
+    [TagName, SerializeField]
+    private string _playerTag = default;
     [Header("各演出用オブジェクトを割り当ててください")]
     [Tooltip("ステージ開始演出用オブジェクト"), SerializeField]
     private StageStartEventBase _stageStartPerformanceObjct = default;
@@ -13,9 +17,42 @@ public class StageController : MonoBehaviour
 
     private void Awake()
     {
-        // 開始演出用オブジェクトをアクティブに、
+        // ステージの開始処理
         // 完了演出、死亡時演出用オブジェクトを非アクティブにする。
-        _stageStartPerformanceObjct.gameObject.SetActive(true);
+        _stageCompletePerformanceObject.gameObject.SetActive(false);
+        _playerDeadPerformanceObjct.gameObject.SetActive(false);
+        if (GameManager.Instance.StageManager.StageStartMode == StageStartMode.FromTheBeginning)
+        {
+            // チェックポイントの情報をリセット
+            GameManager.Instance.StageManager.IsTouchCheckPoint = false;
+            // 開始演出用オブジェクトをアクティブにする。
+            _stageStartPerformanceObjct.gameObject.SetActive(true);
+        }
+        else if (GameManager.Instance.StageManager.StageStartMode == StageStartMode.JustBefore)
+        {
+            // チェックポイントを一つでも触れていなければ、チェックポイント通過時の状態を復元する。
+            // 一つも触れていなければ、何もしない。
+            // チェックポイントからやり直す
+            if (GameManager.Instance.StageManager.IsTouchCheckPoint)
+            {
+                // プレイヤーオブジェクト取得
+                var player = GameObject.FindGameObjectWithTag(_playerTag);
+                // ポジション設定
+                player.transform.position =
+                    GameManager.Instance.StageManager.LastCheckPointPosition;
+
+
+                // プレイヤーコントローラー取得
+                var playerController = player.GetComponent<PlayerController>();
+                // シリンダーの状態を復元
+                // playerController.Revolver.Cylinder =
+                //     GameManager.Instance.StageManager.CheckPointCylinder;
+                // ガンベルトの状態を復元
+                // playerController.BulletCountManager.BulletCounts =
+                //     GameManager.Instance.StageManager.CheckPointGunBelt;
+            }
+            _stageStartPerformanceObjct.gameObject.SetActive(false);
+        }
     }
 
     /// <summary> ステージの完了演出を再生する </summary>
