9c5884138b8e4a9eccb7ee2145c23a46c0eb7555
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-12 13:22:27 +0900

    [update] 不要な処理をコメントアウト

--- Assets/Game/Prepare/Debug/BulletsCountTest.cs
+++ Assets/Game/Prepare/Debug/BulletsCountTest.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class BulletsCountTest : MonoBehaviour
 {
 #if UNITY_EDITOR
-    [SerializeField]
-    private bool _useTestValue = false;
-    [SerializeField]
-    private int _testStandardBulletCount = 5;
-    [SerializeField]
-    private int _testPenetrateBulletCount = 5;
-    [SerializeField]
-    private int _testReflectBulletCount = 5;
+    //[SerializeField]
+    //private bool _useTestValue = false;
+    //[SerializeField]
+    //private int _testStandardBulletCount = 5;
+    //[SerializeField]
+    //private int _testPenetrateBulletCount = 5;
+    //[SerializeField]
+    //private int _testReflectBulletCount = 5;
 
 
-    private void Awake()
-    {
-        if (_useTestValue)
-        {
-            GameManager.Instance.BulletsCountManager.
-                BulletCountHome[BulletType.StandardBullet].Value =
-                _testStandardBulletCount;
+    //private void Awake()
+    //{
+    //    if (_useTestValue)
+    //    {
+    //        GameManager.Instance.BulletsCountManager.
+    //            BulletCountHome[BulletType.StandardBullet].Value =
+    //            _testStandardBulletCount;
 
-            GameManager.Instance.BulletsCountManager.
-                BulletCountHome[BulletType.PenetrateBullet].Value =
-                _testPenetrateBulletCount;
+    //        GameManager.Instance.BulletsCountManager.
+    //            BulletCountHome[BulletType.PenetrateBullet].Value =
+    //            _testPenetrateBulletCount;
 
-            GameManager.Instance.BulletsCountManager.
-                BulletCountHome[BulletType.ReflectBullet].Value =
-                _testReflectBulletCount;
-        }
-    }
+    //        GameManager.Instance.BulletsCountManager.
+    //            BulletCountHome[BulletType.ReflectBullet].Value =
+    //            _testReflectBulletCount;
+    //    }
+    //}
 #endif
 }
