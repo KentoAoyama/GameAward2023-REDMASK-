b7f6d400c669382658af47fe78b5c4d566888fcd
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-12 13:22:06 +0900

    [update] 不要な処理をコメントアウト

--- Assets/Game/Title/SetBulletsNumberButton.cs
+++ Assets/Game/Title/SetBulletsNumberButton.cs
@@ -8,29 +8,29 @@ using Bullet;
 /// </summary>
 public class SetBulletsNumberButton : MonoBehaviour
 {
-    [SerializeField]
-    private int _standardBulletNumber = 10;
-    [SerializeField]
-    private int _penetrateBulletNumber = 10;
-    [SerializeField]
-    private int _reflectBulletNumber = 10;
+    //[SerializeField]
+    //private int _standardBulletNumber = 10;
+    //[SerializeField]
+    //private int _penetrateBulletNumber = 10;
+    //[SerializeField]
+    //private int _reflectBulletNumber = 10;
 
-    private void Awake()
-    {
-        GetComponent<Button>().onClick.AddListener(SetBulletsNumber);
-    }
-    private void SetBulletsNumber()
-    {
-        GameManager.Instance.BulletsCountManager.
-            BulletCountHome[BulletType.StandardBullet].Value =
-                _standardBulletNumber;
+    //private void Awake()
+    //{
+    //    GetComponent<Button>().onClick.AddListener(SetBulletsNumber);
+    //}
+    //private void SetBulletsNumber()
+    //{
+    //    GameManager.Instance.BulletsCountManager.
+    //        BulletCountHome[BulletType.StandardBullet].Value =
+    //            _standardBulletNumber;
 
-        GameManager.Instance.BulletsCountManager.
-            BulletCountHome[BulletType.PenetrateBullet].Value =
-                _penetrateBulletNumber;
+    //    GameManager.Instance.BulletsCountManager.
+    //        BulletCountHome[BulletType.PenetrateBullet].Value =
+    //            _penetrateBulletNumber;
 
-        GameManager.Instance.BulletsCountManager.
-            BulletCountHome[BulletType.ReflectBullet].Value =
-                _reflectBulletNumber;
-    }
+    //    GameManager.Instance.BulletsCountManager.
+    //        BulletCountHome[BulletType.ReflectBullet].Value =
+    //            _reflectBulletNumber;
+    //}
 }
