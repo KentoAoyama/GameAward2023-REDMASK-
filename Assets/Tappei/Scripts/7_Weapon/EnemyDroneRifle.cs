744fdc6bcc6b0dfeaf5add13cdee2079aa3346d4
SatouTappei <stutpi.signup@gmail.com>
2023-04-13 20:20:44 +0900

    [update] ドローンの銃がプレイヤーの方を向くように変更

--- Assets/Tappei/Scripts/7_Weapon/EnemyDroneRifle.cs
+++ Assets/Tappei/Scripts/7_Weapon/EnemyDroneRifle.cs
@@ -7,11 +7,63 @@ using UnityEngine;
 /// </summary>
 public class EnemyDroneRifle : EnemyRifle
 {
-    // 一定範囲内にプレイヤーが入ったらそっちを向く
-    // ポーズと再開に対応させる
-    // 速度変更に対応させる
+    [Header("シーン上に配置されているプレイヤーのタグ")]
+    [SerializeField, TagName] private string _playerTagName;
+    [Header("敵本体の視界と同じ値を使用するか")]
+    [Tooltip("親オブジェクトにEnemyControllerがアタッチされている場合に有効")]
+    [SerializeField] private bool _isLinkEnemyParams = true;
+
+    private Transform _player;
+    private SightSensor _sightSensor;
+    private float _sightRadius = 10;
+    private float _maxAngle = 360;
+
+    private void Awake()
+    {
+        _sightSensor = GetComponent<SightSensor>();
+
+        if (_isLinkEnemyParams)
+        {
+            InitParams();
+        }
+    }
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag(_playerTagName).transform;
+    }
+
+    private void Update()
+    {
+        float result = _sightSensor.TryGetDistanceToPlayer(_sightRadius, _maxAngle);
+        if (result > SightSensor.PlayerOutSight)
+        {
+            TurnToPlayer();
+        }
+    }
+
+    void TurnToPlayer()
+    {
+        Vector3 dir = _player.transform.position - transform.position;
+        transform.right = dir * transform.localScale.x;
+    }
+
     public override void Attack()
     {
-        
+        // 攻撃処理を書く
+    }
+
+    private void InitParams()
+    {
+        EnemyController enemyController = GetComponentInParent<EnemyController>();
+        if (enemyController != null)
+        {
+            _sightRadius = enemyController.Params.SightRadius;
+            _maxAngle = enemyController.Params.SightAngle;
+        }
+        else
+        {
+            Debug.LogWarning("EnemyControllerが取得できませんでした。デフォルトの値を使用します。");
+        }
     }
 }
