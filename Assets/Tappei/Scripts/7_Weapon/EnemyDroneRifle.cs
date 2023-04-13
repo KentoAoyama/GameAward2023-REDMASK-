f102ab68d55a52f1ffa7a4f3ef8a3cb01d63b2b7
SatouTappei <stutpi.signup@gmail.com>
2023-04-13 18:16:49 +0900

    [add] ドローンの武器用に新しくPrefabとスクリプトを作成

---
+++ Assets/Tappei/Scripts/7_Weapon/EnemyDroneRifle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ドローンが使用する武器のクラス
+/// </summary>
+public class EnemyDroneRifle : EnemyRifle
+{
+    // 一定範囲内にプレイヤーが入ったらそっちを向く
+    // ポーズと再開に対応させる
+    // 速度変更に対応させる
+    public override void Attack()
+    {
+        
+    }
+}
