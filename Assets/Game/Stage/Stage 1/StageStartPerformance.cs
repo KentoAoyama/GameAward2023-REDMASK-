0d7e5b99c562e26ac7c1c1f576d03b6f001cffb3
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-13 11:09:16 +0900

    [fix] 開始, 完了演出用コンポーネントを更新

--- Assets/Game/Stage/Stage 1/StageStartPerformance.cs
+++ Assets/Game/Stage/Stage 1/StageStartPerformance.cs
@@ -1,13 +1,30 @@
 // 日本語対応
 using Cysharp.Threading.Tasks;
+using Player;
+using UnityEngine;
+using UnityEngine.Playables;
 
 /// <summary>
 /// ステージ開始演出用コンポーネント
 /// </summary>
 public class StageStartPerformance : StageStartEventBase
 {
-    protected override UniTask StartPerformance()
+    [SerializeField]
+    private PlayerController _playerController = default;
+    [Header("完了演出開始"), SerializeField]
+    private PlayableDirector _startPerformanceDirector = default;
+    [SerializeField]
+    private PlayableDirector _startPerformanceEndDirector = default;
+
+    private bool _isTimeLineEnd = false;
+
+    protected async override UniTask StartPerformance()
     {
-        return default;
+        _startPerformanceDirector.Play();
+        await UniTask.WaitUntil(() => _playerController.InputManager.IsPressed[InputType.Fire1]);
+        _startPerformanceDirector.Stop();
+        _startPerformanceEndDirector.Play();
+        _startPerformanceEndDirector.stopped += _ => _isTimeLineEnd = true;
+        await UniTask.WaitUntil(() => _isTimeLineEnd);
     }
 }
