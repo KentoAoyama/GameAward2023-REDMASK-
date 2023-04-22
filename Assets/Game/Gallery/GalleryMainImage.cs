1ed094caa16d84f405cb1f448bbee442797ab2e7
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-04-22 13:11:33 +0900

    [update] ギャラリー 表示時, 非表示時にアニメーションを追加

--- Assets/Game/Gallery/GalleryMainImage.cs
+++ Assets/Game/Gallery/GalleryMainImage.cs
@@ -1,13 +1,33 @@
 // 日本語対応
 using UnityEngine;
+using DG.Tweening;
 
 public class GalleryMainImage : MonoBehaviour
 {
     [SerializeField]
     private GalleryController _galleryController = default;
+    [Header("表示時アニメーション")]
+    [SerializeField]
+    private float _openDuration = default;
+    [SerializeField]
+    private Ease _openEase = default;
+
+    [Header("非表示時アニメーション")]
+    [SerializeField]
+    private float _closeDuration = default;
+    [SerializeField]
+    private Ease _closeEase = default;
+
+    private bool _isCloseAnimPlaying = false;
+
+    private void Awake()
+    {
+        transform.localScale = Vector3.zero;
+    }
 
     private void OnEnable()
     {
+        transform.DOScale(Vector3.one, _openDuration).SetEase(_openEase);
         for (int i = 0; i < _galleryController.GalleryImages.Length; i++)
         {
             _galleryController.GalleryImages[i].Button.enabled = false;
@@ -16,5 +36,15 @@ public class GalleryMainImage : MonoBehaviour
     private void OnDisable()
     {
         _galleryController.SetupImages();
+    }
+    public void CloseAnimatin()
+    {
+        if (!_isCloseAnimPlaying)
+        {
+            _isCloseAnimPlaying = true;
+            transform.DOScale(Vector3.zero, _closeDuration).
+                SetEase(_closeEase).OnComplete(() =>
+                { gameObject.SetActive(false); _isCloseAnimPlaying = false; });
+        }
     }
 }
