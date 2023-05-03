42aae71b1048935a4569b9e06a103b33c1500622
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-05-03 18:17:21 +0900

    [update]ステージ１の会話シーンを追加

--- Assets/Game/EventScene/PerformanceUtility.cs
+++ Assets/Game/EventScene/PerformanceUtility.cs
@@ -1,7 +1,9 @@
 using Cinemachine;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PerformanceUtility : MonoBehaviour
 {
@@ -10,10 +12,25 @@ public class PerformanceUtility : MonoBehaviour
     private CinemachineImpulseSource _impulse;
     [SerializeField]
     private LineRenderer _line;
+    [SerializeField]
+    private Text _text;
+    [SerializeField]
+    private Image _fadePanel;
 
     private void Start()
     {
         _line.enabled = false;
+        _fadePanel.gameObject.SetActive(true);
+    }
+
+    public void FadeIn(float duration)
+    {
+        _fadePanel.DOFade(0f, duration);
+    }
+
+    public void FadeOut(float duration)
+    {
+        _fadePanel.DOFade(1f, duration);
     }
 
     public void EnemyBrokenSEPlay()
@@ -22,18 +39,35 @@ public class PerformanceUtility : MonoBehaviour
         GameManager.Instance.AudioManager.PlaySE("CueSheet_Gun", "SE_Enemy_Damage");
     }
 
-    public void GunShoot()
+    public void GunShoot(float interval)
     {
         Debug.Log("ŽËŒ‚");
         Impulse();
         GameManager.Instance.AudioManager.PlaySE("CueSheet_Gun", "SE_Player_Attack_Gun");
-        if (_line != null)
-        _line.enabled = true;
 
+        StartCoroutine(Shoot(interval));
     }
 
-    private void Impulse()
+    private IEnumerator Shoot(float interval)
+    {
+        if (_line == null) yield break;
+        _line.enabled = true;
+        yield return new WaitForSeconds(interval);
+        _line.enabled = false;
+    }
+
+    public void Impulse()
     {
         _impulse?.GenerateImpulse();
+    }
+
+    public void FadeInText(float duration)
+    {
+        _text.DOFade(1f, duration);
+    }
+
+    public void FadeOutText(float duration)
+    {
+        _text.DOFade(0f, duration);
     }
 }
--- Assets/Game/Stage/Scripts/Performance/PerformanceEventController.cs
+++ Assets/Game/Stage/Scripts/Performance/PerformanceEventController.cs
@@ -1,20 +1,64 @@
 // 日本語対応
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PerformanceEventController : MonoBehaviour
 {
+    [Header("射撃後までのアニメーション")]
     [SerializeField]
     private PerformanceEvent[] _performanceEvents = default;
+    [Header("射撃後に行う処理")]
+    [SerializeField]
+    public PerformanceEvent[] _afterFirePerformanceEvents = default;
     [SceneName, SerializeField]
     private string _nextSceneName = default;
 
+    private bool _isWaitFireInput;
+    private int _currentIndex = 0;
+    private bool _isAfter = false;
+
     private async void Awake()
     {
-        for (int i = 0; i < _performanceEvents.Length; i++)
+        for (_currentIndex = 0; _currentIndex < _performanceEvents.Length; _currentIndex++)
         {
-            await _performanceEvents[i].Execute();
+            await _performanceEvents[_currentIndex].Execute();
+            if (_performanceEvents[_currentIndex].IsFire) break;
+        }
+        _isAfter = true;
+        for (_currentIndex = 0; _currentIndex < _afterFirePerformanceEvents.Length; _currentIndex++)
+        {
+            await _afterFirePerformanceEvents[_currentIndex].Execute();
         }
         SceneManager.LoadScene(_nextSceneName);
+    }
+
+    private void Update()
+    {
+        if (_isAfter && _currentIndex > -1 && _currentIndex < _afterFirePerformanceEvents.Length)
+        {
+            _afterFirePerformanceEvents[_currentIndex].Update();
+        }
+        else if(_currentIndex > -1 && _currentIndex < _performanceEvents.Length)
+        {
+            _performanceEvents[_currentIndex].Update();
+        }
+    }
+
+    /// <summary>
+    /// 発砲ボタンが押下されるのを待つ。（左クリックかゲームパッドのRightShoulder）
+    /// </summary>
+    /// <returns></returns>
+    private bool WaitInput()
+    {
+        if (Mouse.current != null)
+        {
+            return Mouse.current.leftButton.wasPressedThisFrame;
+        }
+        if (Gamepad.current != null)
+        {
+            return Gamepad.current.rightShoulder.wasPressedThisFrame;
+        }
+        return false;
     }
 }
