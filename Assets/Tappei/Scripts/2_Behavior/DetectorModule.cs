18bb183201c3aa1f68fc6bc5afba795a5ea9579e
SatouTappei <stutpi.signup@gmail.com>
2023-05-07 15:33:37 +0900

    [update] 各行動で飛ばすRayの可視化を切り替えられるように変更

--- Assets/Tappei/Scripts/2_Behavior/DetectorModule.cs
+++ Assets/Tappei/Scripts/2_Behavior/DetectorModule.cs
@@ -13,8 +13,11 @@ public class DetectorModule
         public float _distance;
         public Vector3 _offset;
         public LayerMask _layerMask;
+        public bool _isVisible;
     }
 
+    [Tooltip("このRayは左右の向きに応じて反転しないので注意")]
+    [SerializeField] private RaySettings _onGroundIdleRaySettings;
     [Tooltip("このRayは左右の向きに応じて反転しないので注意")]
     [SerializeField] private RaySettings _footPosRaySettings;
     [Tooltip("このRayは左右の向きに応じて反転しないので注意")]
@@ -26,11 +29,28 @@ public class DetectorModule
     /// 足元(真下)に向かってRayを飛ばすことで床の上にいるかを判定する
     /// 床の上にいる場合はその座標を返すことでキャラクターの足元の基準になる座標を更新する
     /// </summary>
-    public bool DetectFootPos(Transform transform, out Vector3 hitPos)
+    public bool DetectOnGroundIdle(Vector3 center, out Vector3 hitPos)
     {
-        Vector3 rayOrigin = transform.position + _footPosRaySettings._offset;
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector3.down, 
-            _footPosRaySettings._distance, _footPosRaySettings._layerMask);
+        return DetectOnGround(center, out hitPos, _onGroundIdleRaySettings);
+    }
+
+    /// <summary>
+    /// 足元(真下)に向かってRayを飛ばすことで床の上にいるかを判定する
+    /// Idle状態とSearch状態で別々の設定を使用するのでメソッドを分けている
+    /// </summary>
+    public bool DetectFootPos(Vector3 center, out Vector3 hitPos)
+    {
+        return DetectOnGround(center, out hitPos, _footPosRaySettings);
+    }
+
+    private bool DetectOnGround(Vector3 center, out Vector3 hitPos, RaySettings settings)
+    {
+        Vector3 rayOrigin = center + settings._offset;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector3.down, settings._distance, settings._layerMask);
+
+#if UNITY_EDITOR
+        DebugDrawRay(hit, rayOrigin, Vector3.down * settings._distance, settings._isVisible);
+#endif
 
         hitPos = hit.collider ? hit.point : Vector3.zero;
         return hit.collider;
@@ -50,7 +70,7 @@ public class DetectorModule
             _floorRaySettings._distance, _floorRaySettings._layerMask);
 
 #if UNITY_EDITOR
-        DebugDrawRay(hit, rayOrigin, rayDir * _floorRaySettings._distance);
+        DebugDrawRay(hit, rayOrigin, rayDir * _floorRaySettings._distance, _floorRaySettings._isVisible);
 #endif
 
         return hit;
@@ -69,14 +89,17 @@ public class DetectorModule
             _enemyRaySettings._distance, _enemyRaySettings._layerMask);
 
 #if UNITY_EDITOR
-        DebugDrawRay(!hit, rayOrigin, rayDir * _enemyRaySettings._distance);
+        DebugDrawRay(!hit, rayOrigin, rayDir * _enemyRaySettings._distance, _enemyRaySettings._isVisible);
 #endif
 
         return !hit;
     }
 
-    private void DebugDrawRay(bool hit, Vector3 rayOrigin, Vector3 dir)
+    private void DebugDrawRay(bool hit, Vector3 rayOrigin, Vector3 dir, bool isVisible)
     {
+        // 各メソッドで重複して条件分岐を書かなくても良いようにメソッド内で条件分岐を書いている
+        if (!isVisible) return;
+
         Color color = hit ? Color.green : Color.red;
         Debug.DrawRay(rayOrigin, dir * _enemyRaySettings._distance, color, 0.016f);
     }
