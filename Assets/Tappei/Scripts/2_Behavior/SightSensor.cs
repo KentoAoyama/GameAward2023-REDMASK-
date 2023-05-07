9214a324c32795bce6837aefbfef4528914f285f
SatouTappei <stutpi.signup@gmail.com>
2023-05-07 18:15:54 +0900

    [fix] 視界の機能の呼び出し側が複雑な手順を踏んでいたのでそれを解消

--- Assets/Tappei/Scripts/2_Behavior/SightSensor.cs
+++ Assets/Tappei/Scripts/2_Behavior/SightSensor.cs
@@ -6,9 +6,6 @@ using UnityEngine;
 /// </summary>
 public class SightSensor : MonoBehaviour
 {
-    /// <summary>プレイヤーが視界内にいない場合に返る値</summary>
-    public static readonly int PlayerOutSight = -1;
-
     /// <summary>
     /// 一度に視界が検出できるオブジェクトの最大数
     /// ステージに大量の検出できるオブジェクトが存在する場合は増やす必要がある
@@ -20,28 +17,52 @@ public class SightSensor : MonoBehaviour
     [SerializeField] private Transform _eyeTransform;
     [Header("検出するオブジェクトが属するレイヤー")]
     [SerializeField] private LayerMask _playerLayerMask;
-    [Tooltip("視界を遮るオブジェクトのレイヤー")]
+    [Tooltip("視界を遮るオブジェクトのレイヤーを割り当てる")]
     [SerializeField] private LayerMask _obstacleLayerMask;
 
     private Collider2D[] _detectedResults = new Collider2D[MaxDetected];
 
-#if UNITY_EDITOR
-    /// <summary>EnemyControllerでギズモに表示する用途で使っている</summary>
-    public Transform EyeTransform => _eyeTransform;
-#endif
+    /// <summary>
+    /// プレイヤーとの位置関係を列挙型で返す
+    /// このメソッドを外部から呼ぶことでプレイヤーとの位置に応じた処理を行う
+    /// </summary>
+    public SightResult LookForPlayerInSight(float radius, float maxAngle, float attackRange,
+        bool isIgnoreObstacle = false)
+    {
+        if (TryGetDistanceToPlayer(radius, maxAngle, out float result, isIgnoreObstacle))
+        {
+            if (result <= attackRange)
+            {
+                return SightResult.InAttackRange;
+            }
+            else
+            {
+                return SightResult.InSight;
+            }
+        }
+        else
+        {
+            return SightResult.OutSight;
+        }
+    }
 
     /// <returns>
     /// プレイヤーが視界内にいる場合はプレイヤーとの距離を返す
-    /// 視界内にいない場合は PlayerOutSight が返る
+    /// 視界内にいない場合は-1が返る
     /// </returns>
-    public float TryGetDistanceToPlayer(float radius, float maxAngle, bool isIgnoreObstacle = false)
+    private bool TryGetDistanceToPlayer(float radius, float maxAngle, out float result, 
+        bool isIgnoreObstacle = false)
     {
         Vector3 rayOrigin = _eyeTransform.position;
 
         // ヒットしなかった場合でも配列内の要素は削除されないので
         // ヒットしたオブジェクトの情報を返すように変更する場合は注意
         int hitCount = Physics2D.OverlapCircleNonAlloc(rayOrigin, radius, _detectedResults, _playerLayerMask);
-        if (hitCount == 0) return PlayerOutSight;
+        if (hitCount == 0)
+        {
+            result = -1;
+            return false;
+        }
 
         foreach (Collider2D detectedCollider in _detectedResults)
         {
@@ -56,7 +77,11 @@ public class SightSensor : MonoBehaviour
             float distance = Vector3.Distance(rayOrigin, targetPos);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, targetDir, distance, _obstacleLayerMask);
 
-            if (isIgnoreObstacle) return distance;
+            if (isIgnoreObstacle)
+            {
+                result = distance;
+                return true;
+            }
 
             // 視界を遮るオブジェクト用のレイヤーがあれば、ターゲットまでのRayを飛ばして
             // 視界を遮るオブジェクトにヒットしたら視界に映らないという処理に変更出来る。
@@ -67,9 +92,14 @@ public class SightSensor : MonoBehaviour
             Color color = isSightable ? Color.green : Color.red;
             Debug.DrawRay(rayOrigin, targetDir * distance, color);
 #endif
-            if (isSightable) return distance;
+            if (isSightable)
+            {
+                result = distance;
+                return true;
+            }
         }
 
-        return PlayerOutSight;
+        result = -1;
+        return false;
     }
 }
