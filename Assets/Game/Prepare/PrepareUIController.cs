339af9348a29ed6b93129934350d699736bc4ff9
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-05-08 15:04:17 +0900

    [update] 準備画面 UI Manager を更新

--- Assets/Game/Prepare/PrepareUIController.cs
+++ Assets/Game/Prepare/PrepareUIController.cs
@@ -16,11 +16,32 @@ public class PrepareUIController : MonoBehaviour
     [SerializeField]
     private Button[] _rightButtons = default;
 
+    [SerializeField]
+    private Button[] _cylinderButtonNavigation = default;
+    [SerializeField]
+    private Button[] _gunbeltButtonNavigation = default;
+
     private GameObject _previousSelectedObj = null;
 
     private void Awake()
     {
         _prepareAnimation.CurrentScreenArea.Subscribe(ChangeScreenArea);
+    }
+    private void Update()
+    {
+        // 選択オブジェクトがなくなったら前のやつを選択しなおす。
+        if (_eventSystem.currentSelectedGameObject == null)
+        {
+            _eventSystem.SetSelectedGameObject(_previousSelectedObj);
+        }
+        if (_previousSelectedObj != _eventSystem.currentSelectedGameObject)
+        {
+            if (_eventSystem.currentSelectedGameObject.TryGetComponent(out ButtonNavigationController buttonNavigationController))
+            {
+                buttonNavigationController.Setting();
+            }
+        }
+        _previousSelectedObj = _eventSystem.currentSelectedGameObject;
     }
     private void ChangeScreenArea(PrepareImageSlideController.ScreenArea screenArea)
     {
@@ -53,30 +74,44 @@ public class PrepareUIController : MonoBehaviour
         // ============= 左側の停止処理 ============= //
 
     }
-    private void Update()
-    {
-        // 選択オブジェクトがなくなったら前のやつを選択しなおす。
-        if (_eventSystem.currentSelectedGameObject == null)
-        {
-            _eventSystem.SetSelectedGameObject(_previousSelectedObj);
-        }
-        _previousSelectedObj = _eventSystem.currentSelectedGameObject;
-    }
 
-    public GameObject GetNearSelectableObj(Transform origin)
+    public GameObject GetNearSelectableObjForCylinder(Transform origin)
     {
-        for (int i = 0; i < _rightButtons.Length; i++)
+        for (int i = 0; i < _cylinderButtonNavigation.Length; i++)
         {
             // 自分を探す
-            if (origin.gameObject == _rightButtons[i].gameObject)
+            if (origin.gameObject == _cylinderButtonNavigation[i].gameObject)
             {
                 // 自分と近い 選択可能な オブジェクトを探す
                 // 近いとは登録されたインデックスの数値が近いことを指す
-                for (int j = i + 1; j < _rightButtons.Length + i; j++)
+                for (int j = i + 1; j < _cylinderButtonNavigation.Length + i; j++)
                 {
-                    int index = j % _rightButtons.Length;
-                    if (!_rightButtons[index].interactable) continue;
-                    return _rightButtons[index].gameObject;
+                    int index = j % _cylinderButtonNavigation.Length;
+                    if (!_cylinderButtonNavigation[index].interactable) continue;
+                    return _cylinderButtonNavigation[index].gameObject;
+                }
+                // 検索に失敗した場合 nullを返す。
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public GameObject GetNearSelectableObjForGunbelt(Transform origin)
+    {
+        for (int i = 0; i < _gunbeltButtonNavigation.Length; i++)
+        {
+            // 自分を探す
+            if (origin.gameObject == _gunbeltButtonNavigation[i].gameObject)
+            {
+                // 自分と近い 選択可能な オブジェクトを探す
+                // 近いとは登録されたインデックスの数値が近いことを指す
+                for (int j = i + 1; j < _gunbeltButtonNavigation.Length + i; j++)
+                {
+                    int index = j % _gunbeltButtonNavigation.Length;
+                    if (!_gunbeltButtonNavigation[index].interactable) continue;
+                    return _gunbeltButtonNavigation[index].gameObject;
                 }
                 // 検索に失敗した場合 nullを返す。
                 return null;
