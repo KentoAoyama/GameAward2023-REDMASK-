9d9edc7e5867dfeb933bafff09f00a00edcaba1e
SatouTappei <stutpi.signup@gmail.com>
2023-05-11 16:28:32 +0900

    [add] メッセージングをするためのクラスの追加

---
+++ Assets/Tappei/Scripts/8_MessageSystem/ReactionMessageSender.cs
@@ -0,0 +1,14 @@
+﻿using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの任意の行動をトリガーに敵を向かわせるクラス
+/// 送る側
+/// </summary>
+public static class ReactionMessageSender
+{
+    public static void SendMessage(Transform transform)
+    {
+        MessageBroker.Default.Publish(new ReactionMessage(transform.position));
+    }
+}
