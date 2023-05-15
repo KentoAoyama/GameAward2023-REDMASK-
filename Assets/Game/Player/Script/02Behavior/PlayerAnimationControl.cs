3ecbd8c94743fbfa78a925f227ca9de1eb902512
Aiba_0325 <bantan.ab325@gmail.com>
2023-05-15 20:25:33 +0900

    [Add] 回避中のアニメーションの再生、構え発砲。カメラの種類の追加

--- Assets/Game/Player/Script/02Behavior/PlayerAnimationControl.cs
+++ Assets/Game/Player/Script/02Behavior/PlayerAnimationControl.cs
@@ -15,40 +15,43 @@ namespace Player
         [Tooltip("絵が右向きならTrueに"), SerializeField]
         private bool _isRightDirOnPictuer = true;
 
-
-
+        [Header("=====オブジェクトの設定======")]
         [Header("構えのオブジェクト")]
-        [SerializeField] private List<GameObject> _nomalAnim = new List<GameObject>();
-
+        [SerializeField] private List<GameObject> _nomalGunSetUpObjects = new List<GameObject>();
         [Header("回避中の構えのオブジェクト")]
-        [SerializeField] private List<GameObject> _avoidAnim = new List<GameObject>();
-
+        [SerializeField] private List<GameObject> _avoidGunSetUpObjects = new List<GameObject>();
+        [Header("回避中の構えの左向きのオブジェクト")]
+        [SerializeField] private GameObject _avoidLeftBody;
+        [Header("回避中の構えの右向きのオブジェクト")]
+        [SerializeField] private GameObject _avoidRightBody;
         [Header("通常のオブジェクト")]
-        [SerializeField] private GameObject _fireAnim;
+        [SerializeField] private GameObject _animationObject;
 
-        [Header("Animation名")]
-
-
-
+        [Header("=====Animation名======")]
         [Header("ジャンプ")]
         [SerializeField] private string _animJump = "Player_JumpStart";
+        [Header("近接攻撃")]
+        [SerializeField] private string _animProximity = "Player_Proximity";
         [Header("発砲")]
         [SerializeField] private string _animFire = "Player_Fire";
+        [Header("回避")]
+        [SerializeField] private string _animAvoid = "Player_Avoid";
+        [Header("回避中に発砲")]
+        [SerializeField] private string _animAvoidFire = "Player_Fire";
         [Header("リロードはじめ")]
         [SerializeField] private string _animReLoadStart = "Player_ReLoadStart";
         [Header("リロード、弾を籠める")]
         [SerializeField] private string _animReLoadNow = "Player_ReLoadNow";
         [Header("リロード、終わり")]
         [SerializeField] private string _animReLoadEnd = "Player_ReLoadEnd";
-        [Header("近接攻撃")]
-        [SerializeField] private string _animProximity = "Player_Proximity";
-        [Header("回避")]
-        [SerializeField] private string _animAvoid = "Player_Avoid";
+        [Header("死亡")]
+        [SerializeField] private string _animDead = "Player_Dead";
 
-        [Header("Animatorのパラメータ名")]
+
+
+        [Header("====Animatorのパラメータ名====")]
         [Header("速度X")]
         private string _xVelocityParameta = "SpeedX";
-
         [Header("速度Y")]
         private string _yVelocityParameta = "SpeedY";
         [Header("設置")]
@@ -62,15 +65,20 @@ namespace Player
         /// <summary>現在のキャラの移動の入力の向き</summary>
         private float _moveHorizontalDir = 1;
 
+        /// <summary>発砲のアニメーション再生中かどうか</summary>
         private bool _isFire = false;
 
+        /// <summary>現在アニメーション再生中かどうか</summary>
         private bool _isAnimationNow = false;
+
+
         private PlayerController _playerController;
 
         public bool IsAnimationNow => _isAnimationNow;
         public float MoveDir { get => _moveHorizontalDir; set => _moveHorizontalDir = value; }
         public bool IsPause { get; private set; } = false;
 
+    /// <summary>再生させるアニメーションの種類</summary>
         public enum AnimaKind
         {
             /// <summary>発砲</summary>
@@ -81,10 +89,16 @@ namespace Player
             Jump,
             /// <summary>回避</summary>
             Avoid,
-
+            /// <summary>回避中に、発砲</summary>
+            AvoidFire,
+            /// <summary>リロード開始</summary>
             ReLoadStart,
+            /// <summary>リロード、弾籠め</summary>
             ReLoad,
+            /// <summary>リロード、終了</summary>
             ReLoadEnd,
+            /// <summary>死亡</summary>
+            Dead,
         }
 
         public void Init(PlayerController playerController)
@@ -114,7 +128,7 @@ namespace Player
             }
         }
 
-
+        /// <summary>アニメーターのパラメータを設定</summary>
         public void SetAnimatorParameters()
         {
             float x = Mathf.Abs(_playerController.Rigidbody2D.velocity.x);
@@ -126,57 +140,94 @@ namespace Player
             _playerController.PlayerAnim.SetBool(_isGroundParameta, _playerController.GroungChecker.IsHit(_playerController.Move.MoveHorizontalDir));
         }
 
+        /// <summary>構えのObjectを表示し、アニメーション用のObjectを非表示にする</summary>
+        /// <param name="isAvoid"></param>
         public void GunSet(bool isAvoid)
         {
-            _fireAnim.SetActive(false);
-            Debug.Log(_playerController.Avoidance.IsAvoidanceNow);
+            //アニメーション再生中は構えは出来ない
+            if (_isAnimationNow) return;
 
+            //アニメーション用のオブジェクトを非表示にする
+            _animationObject.SetActive(false);
+
+
+            //回避中は回避用の構えを表示
             if (_playerController.Avoidance.IsAvoidanceNow)
             {
+                //回避用の構えのオブジェクトを表示
+                _avoidGunSetUpObjects.ForEach(i => i.SetActive(true));
+                //通常の構えのオブジェクトを非表示
+                _nomalGunSetUpObjects.ForEach(i => i.SetActive(false));
 
-                _avoidAnim.ForEach(i => i.SetActive(true));
-                _nomalAnim.ForEach(i => i.SetActive(false));
-            }
+                //プレイヤーの向きによって、右向き左向きを切り替える
+                if (_playerController.Player.transform.localScale.x == 1)
+                {
+                    _avoidRightBody.SetActive(true);
+                    _avoidLeftBody.SetActive(false);
+                }
+                else
+                {
+                    _avoidRightBody.SetActive(false);
+                    _avoidLeftBody.SetActive(true);
+                }
+            }    //通常の構えを表示
             else
             {
 
                 if (_playerController.GunSetUp.IsGunSetUp)
                 {
-                    _avoidAnim.ForEach(i => i.SetActive(false));
-                    _nomalAnim.ForEach(i => i.SetActive(true));
+                    //回避の構えのオブジェクトを非表示
+                    _avoidGunSetUpObjects.ForEach(i => i.SetActive(false));
+                    //通常の構えのオブジェクトを表示
+                    _nomalGunSetUpObjects.ForEach(i => i.SetActive(true));
                 }
             }
-
-
         }
 
+        /// <summary>構えが終わった時に構えのオブジェクトを非表示にする</summary>
         public void GunSetEnd()
         {
+            //アニメーション用のオブジェクトを表示
+            _animationObject.SetActive(true);
 
-            _fireAnim.SetActive(true);
+            //それぞれの構えのオブジェクトを非表示
             if (_playerController.GunSetUp.IsGunSetUp)
             {
-                _avoidAnim.ForEach(i => i.SetActive(false));
-                _nomalAnim.ForEach(i => i.SetActive(false));
+                _avoidGunSetUpObjects.ForEach(i => i.SetActive(false));
+                _nomalGunSetUpObjects.ForEach(i => i.SetActive(false));
             }
         }
 
+        /// <summary>回避のアニメーションパラメーター設定</summary>
+        /// <param name="isTrue"></param>
         public void SetBoolAvoid(bool isTrue)
         {
             _playerController.PlayerAnim.SetBool("IsAvoid", isTrue);
         }
 
 
+        /// <summary>プレイヤーの回避のアニメーションを再生</summary>
+        /// <param name="animationKind">再生したいアニメーション</param>
         public void PlayAnimation(PlayerAnimationControl.AnimaKind animationKind)
         {
+            //現在アニメーション再生中
             _isAnimationNow = true;
-            _fireAnim.SetActive(true);
 
+            //アニメーション用のオブジェクトを表示
+            _animationObject.SetActive(true);
 
-            _nomalAnim.ForEach(i => i.SetActive(false));
+            //構えのオブジェクトを非表示
+            _nomalGunSetUpObjects.ForEach(i => i.SetActive(false));
+            _avoidGunSetUpObjects.ForEach(i => i.SetActive(false));
+
 
             if (animationKind == AnimaKind.Fire)
             {
+                if (_playerController.GunSetUp.IsGunSetUp)
+                {
+                    _animationObject.transform.localScale = new Vector3(_playerController.BodyAnglSetteing.AnimationScaleX * _playerController.Player.transform.localScale.x, 1, 1);
+                }
+
                 _playerController.PlayerAnim.Play(_animFire);
                 _isFire = true;
             }
@@ -208,29 +259,56 @@ namespace Player
             }
             else if (animationKind == AnimaKind.Avoid)
             {
+                if (_playerController.GunSetUp.IsGunSetUp)
+                {
+                    _animationObject.transform.localScale = new Vector3(_playerController.BodyAnglSetteing.AnimationScaleX * _playerController.Player.transform.localScale.x, 1, 1);
+                }
                 _playerController.PlayerAnim.Play(_animAvoid);
                 _playerController.PlayerAnim.SetBool("IsAvoid", true);
                 _isFire = true;
             }
+            else if (animationKind == AnimaKind.AvoidFire)
+            {
+                if (_playerController.GunSetUp.IsGunSetUp)
+                {
+                    _animationObject.transform.localScale = new Vector3(_playerController.BodyAnglSetteing.AnimationScaleX * _playerController.Player.transform.localScale.x, 1, 1);
+                }
+
+                _playerController.PlayerAnim.Play(_animAvoidFire);
+                _isFire = true;
+            }
+            else if(animationKind == AnimaKind.Dead)
+            {
+                _playerController.PlayerAnim.Play(_animDead);
+            }
         }
 
+        /// <summary>アニメーションが終わったことを通知</summary>
         public void EndAnimation()
         {
+
+            //構えていたらそれに対応したオブジェクト非表示
             if (_playerController.GunSetUp.IsGunSetUp)
             {
                 if (_playerController.Avoidance.IsAvoidanceNow)
                 {
-                    _avoidAnim.ForEach(i => i.SetActive(true));
-                    _nomalAnim.ForEach(i => i.SetActive(false));
+                    _avoidGunSetUpObjects.ForEach(i => i.SetActive(true));
+                    _nomalGunSetUpObjects.ForEach(i => i.SetActive(false));
                 }
                 else
                 {
-                    _avoidAnim.ForEach(i => i.SetActive(false));
-                    _nomalAnim.ForEach(i => i.SetActive(true));
+                    _avoidGunSetUpObjects.ForEach(i => i.SetActive(false));
+                    _nomalGunSetUpObjects.ForEach(i => i.SetActive(true));
                 }
-
             }
+
+            //アニメーション用のオブジェクトを元に戻す
+            _animationObject.transform.localScale = new Vector3(1, 1, 1);
+
+            //現在アニメーション再生中ではない
             _isAnimationNow = false;
+
+            //発砲中ではない
             _isFire = false;
         }
 
@@ -256,18 +334,10 @@ namespace Player
             }
         }
 
-
-
-        public void PlayAnim()
-        {
-
-        }
-
         /// <summary>死亡アニメーションを再生</summary>
         public void Dead()
         {
             _playerController.PlayerAnim.Play(_deadAnimName);
         }
-
     }
 }
