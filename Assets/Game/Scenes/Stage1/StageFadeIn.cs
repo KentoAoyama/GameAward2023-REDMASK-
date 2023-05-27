1b4cc123ed0809f7299c957411165ecb4f1772d6
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-05-27 10:50:23 +0900

    [add] フェードイン, アウト用プレハブを作成

--- Assets/Game/Scenes/Stage1/StageFadeIn.cs
+++ Assets/Game/Scenes/Stage1/StageFadeIn.cs
@@ -3,6 +3,7 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class StageFadeIn : MonoBehaviour
@@ -13,11 +14,17 @@ public class StageFadeIn : MonoBehaviour
     private float _time = default;
     [SerializeField]
     private Ease _ease = default;
+    [SerializeField]
+    private UnityEvent _startAction = default;
+    [SerializeField]
+    private UnityEvent _onComplete = default;
 
     public async void FadeIn(Action startAction = null, Action onComplete = null)
     {
         startAction?.Invoke();
+        _startAction.Invoke();
         await _targetImage.DOFade(0f, _time).SetEase(_ease);
         onComplete?.Invoke();
+        _onComplete.Invoke();
     }
 }
--- Assets/Game/Scenes/Stage1/StageFadeOut.cs
+++ Assets/Game/Scenes/Stage1/StageFadeOut.cs
@@ -3,6 +3,7 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class StageFadeOut : MonoBehaviour
@@ -13,11 +14,17 @@ public class StageFadeOut : MonoBehaviour
     private float _time = default;
     [SerializeField]
     private Ease _ease = default;
+    [SerializeField]
+    private UnityEvent _startAction = default;
+    [SerializeField]
+    private UnityEvent _onComplete = default;
 
-    public async void FadeOut(Action startAction = null, Action onComplete = null)
+    public async UniTask FadeOut(Action startAction = null, Action onComplete = null)
     {
         startAction?.Invoke();
+        _startAction.Invoke();
         await _targetImage.DOFade(1f, _time).SetEase(_ease);
         onComplete?.Invoke();
+        _onComplete.Invoke();
     }
 }
